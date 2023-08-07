741cab152bcfaf672380c429aefb6ef57c970d30
Sayman Rifat <98202630+saymanrifat@users.noreply.github.com>
2023-08-07 21:41:38 +0600

    first commit

---
+++ lib/pages/welcome_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class WelcomePage extends StatefulWidget {
+  const WelcomePage({super.key});
+
+  @override
+  State<WelcomePage> createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  List<String> images = [
+    "welcome-one.png",
+    "welcome-two.png",
+    "welcome-three.png",
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView.builder(
+        scrollDirection: Axis.vertical,
+        itemCount: images.length,
+        itemBuilder: (context, index) {
+          return Container(
+            width: double.maxFinite,
+            height: double.maxFinite,
+            decoration: BoxDecoration(
+                image:
+                    DecorationImage(image: AssetImage('img/${images[index]}'))),
+          );
+        },
+      ),
+    );
+  }
+}
