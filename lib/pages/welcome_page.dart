bb7570cee0e79b8195cdd037eb4d0b8f1e6b112f
Sayman Rifat <98202630+saymanrifat@users.noreply.github.com>
2023-08-08 10:41:59 +0600

    Welcome Screen Done

--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_project_app/misc/colors.dart';
+import 'package:flutter_project_app/widgets/app_large_text.dart';
+import 'package:flutter_project_app/widgets/app_text.dart';
+import 'package:flutter_project_app/widgets/responsive_button.dart';
 
 class WelcomePage extends StatefulWidget {
   const WelcomePage({super.key});
@@ -24,8 +28,64 @@ class _WelcomePageState extends State<WelcomePage> {
             width: double.maxFinite,
             height: double.maxFinite,
             decoration: BoxDecoration(
-                image:
-                    DecorationImage(image: AssetImage('img/${images[index]}'))),
+              image: DecorationImage(
+                  image: AssetImage('img/${images[index]}'), fit: BoxFit.cover),
+            ),
+            child: Container(
+              margin: const EdgeInsets.only(top: 150, left: 20, right: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      AppLargeText(
+                        text: "Trips",
+                      ),
+                      AppText(
+                        text: 'Mountain',
+                        size: 30,
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Container(
+                        width: 250,
+                        child: AppText(
+                          text:
+                              "Mountain hikes give you an incredable sence of freedom along with enduance text,",
+                          color: AppColors.textColor2,
+                          size: 14,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      ResponsiveButton(
+                        width: 120,
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: List.generate(
+                      3,
+                      (indexSlider) {
+                        return Container(
+                          margin: EdgeInsets.only(bottom: 2),
+                          width: 8,
+                          height: index == indexSlider ? 25 : 8,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(8),
+                              color: index == indexSlider
+                                  ? AppColors.mainColor
+                                  : AppColors.mainColor.withOpacity(0.3)),
+                        );
+                      },
+                    ),
+                  )
+                ],
+              ),
+            ),
           );
         },
       ),
